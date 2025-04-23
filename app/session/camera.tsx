45e14d8cba0bc0e6451d99aca97108e8d5401b1e
Luca Manuel De Veintemilla Altamirano <lmde2@espe.edu.ec>
2025-04-23 11:01:28 -0500

    Puntaje eliminado por estudiante

--- app/session/camera.tsx
+++ app/session/camera.tsx
@@ -1,7 +1,6 @@
 import { useSession } from "@/context/SessionContext";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 import { useState, useEffect } from "react";
-import { format } from "date-fns";
 import {
   Button,
   Modal,
@@ -10,28 +9,15 @@ import {
   StyleSheet,
   Image,
   Pressable,
-  ScrollView,
-  TextInput,
   Keyboard,
   TouchableWithoutFeedback,
-  TouchableOpacity,
 } from "react-native";
 import * as ImageManipulator from "expo-image-manipulator";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Dropdown } from "react-native-element-dropdown";
-import { AntDesign } from "@expo/vector-icons";
 import { User } from "@/context/AuthContext";
-import { ThemedView } from "@/components/ThemedView";
-import { ThemedText } from "@/components/ThemedText";
-import Slider from "@react-native-community/slider";
-
-type Score = {
-  student: User;
-  score: string;
-  timestamp: string;
-};
 
 const CameraModal = ({
   classroomStudents,
@@ -55,9 +41,6 @@ const CameraModal = ({
   const [permission, requestPermission] = useCameraPermissions();
   const [currentPicture, setCurrentPicture] = useState<string | null>(null);
   const [cameraRef, setCameraRef] = useState<any>(null);
-
-  const [score, setScore] = useState<string>("");
-  const [scoresList, setScoresList] = useState<Score[]>([]);
 
   const [selectedStudent, setSelectedStudent] = useState<User | null>(null);
 
@@ -104,27 +87,11 @@ const CameraModal = ({
     setFacing((current) => (current === "back" ? "front" : "back"));
   }
 
-  const handleSetScore = () => {
-    if (selectedStudent && score) {
-      setScoresList((prev) => [
-        ...prev,
-        {
-          student: selectedStudent,
-          score: score,
-          timestamp: new Date().toISOString(),
-        },
-      ]);
-      setSelectedStudent(null);
-      setScore("");
-    }
-  };
-
   if (!permission) {
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
         <Text style={styles.message}>
@@ -161,27 +128,12 @@ const CameraModal = ({
                 style={styles.camera}
               />
             </View>
-            
             <View style={styles.pictureContainer}>
               <Image
                 source={{ uri: currentPicture ?? "" }}
                 style={styles.picture}
               />
             </View>
-
-            <View style={styles.controlsContainer}>
-              <Pressable
-                disabled={!selectedStudent}
-                onPress={isRecording ? handleStopRecording : handleStartRecording}
-              >
-                <IconSymbol
-                  size={40}
-                  name={isRecording ? "stop" : "play"}
-                  color={!selectedStudent ? "gray" : isRecording ? "red" : "green"}
-                />
-              </Pressable>
-            </View>
-
             <View style={styles.scoreInputContainer}>
               <Dropdown
                 style={styles.dropdown}
@@ -203,41 +155,18 @@ const CameraModal = ({
                   setSelectedStudent(student || null);
                 }}
               />
-              <View style={styles.sliderContainer}>
-                <Slider
-                  style={{ flex: 1 }}
-                  minimumValue={0}
-                  maximumValue={5}
-                  step={1}
-                  value={Number(score)}
-                  onValueChange={(value) => setScore(value.toString())}
-                  minimumTrackTintColor="#2196F3"
-                  maximumTrackTintColor="#000001"
-                  thumbTintColor="#2196F3"
-                />
-              </View>
-              <Pressable 
-                onPress={handleSetScore}
-                style={styles.addScoreButton}
-                disabled={!selectedStudent || !score}
+            </View>
+            <View style={styles.controlsContainer}>
+              <Pressable
+                disabled={!selectedStudent}
+                onPress={isRecording ? handleStopRecording : handleStartRecording}
               >
-                <IconSymbol 
-                  size={30} 
-                  name="plus.app" 
-                  color={(!selectedStudent || !score) ? "gray" : "green"} 
+                <IconSymbol
+                  size={40}
+                  name={isRecording ? "stop" : "play"}
+                  color={!selectedStudent ? "gray" : isRecording ? "red" : "green"}
                 />
               </Pressable>
-            </View>
-
-            <View style={styles.scoresContainer}>
-              <ScrollView 
-                style={styles.scoresList}
-                contentContainerStyle={styles.scoresListContent}
-              >
-                {scoresList.map((item, i) => (
-                  <StudentCard score={item} key={i} />
-                ))}
-              </ScrollView>
             </View>
           </View>
         </View>
@@ -299,19 +228,23 @@ const styles = StyleSheet.create({
     marginTop: 80,
   },
   cameraContainer: {
-    height: '25%',
+    flex: 0.5,
     width: '100%',
     borderWidth: 1,
     borderColor: 'gray',
     backgroundColor: 'lightgray',
-    marginBottom: 5,
+    marginBottom: 10,
+    borderRadius: 8,
+    overflow: 'hidden',
   },
   pictureContainer: {
-    height: '25%',
+    flex: 0.5,
     width: '100%',
     borderWidth: 1,
     borderColor: 'gray',
-    marginBottom: 5,
+    marginBottom: 10,
+    borderRadius: 8,
+    overflow: 'hidden',
   },
   camera: {
     width: "100%",
@@ -333,62 +266,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     gap: 5,
-  },
-  sliderContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 40,
-  },
-  scoreText: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  addScoreButton: {
-    flex: 0.5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 40,
-  },
-  scoresContainer: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#f0f0f0',
-    borderRadius: 8,
-    marginTop: 5,
-  },
-  scoresList: {
-    flex: 1,
-    width: '100%',
-  },
-  scoresListContent: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  setScoreContainer: {
-    height: "auto",
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 10,
-  },
-  textListContainer: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: "lightgray",
-    paddingHorizontal: 10,
-  },
-  listItem: {
-    fontSize: 16,
-    color: "red",
-    paddingVertical: 5,
-  },
-  input: {
-    padding: 5,
-    borderWidth: 1,
-    borderColor: "gray",
-    textAlign: "center",
   },
   dropdown: {
     flex: 2,
@@ -416,77 +293,6 @@ const styles = StyleSheet.create({
     height: 40,
     fontSize: 16,
   },
-  profileImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 10,
-  },
-  containerText: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "center",
-  },
-  studentName: {
-    fontWeight: "bold",
-    fontSize: 16,
-    color: "black",
-  },
-  score: {
-    fontWeight: "600",
-    fontSize: 14,
-    color: "green",
-  },
-  timestamp: {
-    fontSize: 12,
-    color: "gray",
-  },
 });
-
-const StudentCard = ({ score }: { score: Score }) => {
-  const getImageUrl = (path: string) => {
-    return `${process.env.EXPO_PUBLIC_API_URL}/user/${path}`;
-  };
-
-  return (
-    <TouchableOpacity
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "center",
-        padding: 4,
-        borderRadius: 10,
-        gap: 10,
-        marginVertical: 5,
-      }}
-    >
-      <Image
-        source={{
-          uri: score.student ? getImageUrl(score.student.images[0]) : "",
-        }}
-        style={styles.profileImage}
-      />
-      <View style={styles.containerText}>
-        <ThemedText style={styles.studentName}>
-          {score.student.name} {score.student.last_name}
-        </ThemedText>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          <ThemedText style={styles.score}>Score: {score.score}</ThemedText>
-          <ThemedText style={styles.timestamp}>
-            {format(new Date(score.timestamp), "dd/MM/yyyy HH:mm:ss")}
-          </ThemedText>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-};
 
 export default CameraModal;
